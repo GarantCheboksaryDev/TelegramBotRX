13f14d8be20719128593f77a5df0dce35a8d6c43
DirectumRXUser <directumrxuser@c.ru>
2025-03-27 10:17:40 +0300

    Сгенерированные файлы

---
+++ Garant.TelegramBot/Garant.TelegramBot.Shared/BotUser/BotUserHandlers.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Garant.TelegramBot.BotUser;
+
+namespace Garant.TelegramBot
+{
+  partial class BotUserSharedHandlers
+  {
+
+  }
+}
---
+++ Garant.TelegramSolution/Garant.TelegramSolution.Shared/ModuleConstants.cs
@@ -0,0 +1,10 @@
+using System;
+using Sungero.Core;
+
+namespace Garant.TelegramSolution.Constants
+{
+  public static class Module
+  {
+
+  }
+}
